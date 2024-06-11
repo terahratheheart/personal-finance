07aade53d31fe0c8f1c65d975653fec22f0c26fc
terah ratheheart <terahratheheart@jeppy.local>
2024-06-10 19:38:26 -0700

    redirect auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,35 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Dashboard } from './pages/dashboard'
-import { Auth } from './pages/auth'
-import { FinancialRecordsProvider } from './contexts/FinancialRecordContext'
- 
-function App() {
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+import { FinancialRecordsProvider } from "./contexts/FinancialRecordContext";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+// import { dark } from "@clerk/themes";
 
+function App() {
   return (
     <Router>
-      <div className='app-container'>
+      <div className="app-container">
+        <div className="navbar">
+          <Link to="/"> Dashboard</Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
         <Routes>
-          <Route path='/' element={<FinancialRecordsProvider><Dashboard/></FinancialRecordsProvider>}/>
-          <Route path='/auth' element={<Auth/>} />
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
+          />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
-      
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -3,19 +3,20 @@ import {
     SignedOut,
     SignInButton,
     SignUpButton,
-    UserButton
-} from '@clerk/clerk-react'
-
-export const Auth = () => {
-    return(
-        <div className='sign-in-container'>
-            <SignedOut >
-                <SignUpButton mode='modal' />
-                <SignInButton mode='modal'/>
-            </SignedOut>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-        </div>
-    )
-}
+  } from "@clerk/clerk-react";
+  import { Navigate } from "react-router-dom";
+  
+  export const Auth = () => {
+    return (
+      <div className="sign-in-container">
+        <SignedOut>
+          <h1> Welcome to Your Own Personal Finance Tracker!</h1>
+          <SignUpButton mode="modal" />
+          <SignInButton mode="modal" />
+        </SignedOut>
+        <SignedIn>
+          <Navigate to="/" />
+        </SignedIn>
+      </div>
+    );
+  };
