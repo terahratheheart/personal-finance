6e1014ad7d9cbbefedba128458881bf01ea15aed
terah ratheheart <terahratheheart@jeppy.local>
2024-06-10 19:35:27 -0700

    add totals and styling

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,19 +1,28 @@
-
-import {
-    useUser
-   } from '@clerk/clerk-react' 
-import { FinancialRecordForm } from './FinancialRecordForm'
-import { FinancialRecordList } from './FinancialRecordList'
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./FinancialRecordForm";
+import { FinancialRecordList } from "./FinancialRecordList";
 import "./financial-record.css";
-   
-   export const Dashboard = () => {
-       const {user} = useUser()
-       return (
-       <div className='dashboard-container'>
-           <h1>Welcome {user?.firstName}! Here are Yr Finances:</h1>
-           <FinancialRecordForm />
-            <FinancialRecordList />
-       </div>
-   
-       )
-   }
+import { useFinancialRecords } from "../../contexts/FinancialRecordContext";
+import { useMemo } from "react";
+export const Dashboard = () => {
+  const { user } = useUser();
+  const { records } = useFinancialRecords();
+
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += Number(record.amount);
+    });
+
+    return totalAmount;
+  }, [records]);
+
+  return (
+    <div className="dashboard-container">
+      <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
+      <FinancialRecordForm />
+      <div>Total Monthly: ${totalMonthly}</div>
+      <FinancialRecordList />
+    </div>
+  );
+};
